a35afbe7ca1ddd6e95fe8e04e9bfe707b4d404bd
maitoyamada <115780476+maitoyamada@users.noreply.github.com>
2023-04-17 23:03:20 +0900

    Update 5-2.rb

--- 5-2.rb
+++ 5-2.rb
@@ -1,33 +1,37 @@
-input_file = 'bunny.ply'
-output_file = 'output.obj'
+def ply_to_obj(ply_file, obj_file)
+  vertices = []
+  faces = []
+  reading_vertices = false
+  reading_faces = false
+  vertex_count = 0
 
-vertices = []
-faces = []
-
-File.open(input_file, 'r') do |file|
-  file.each_line do |line|
-    if line.start_with?('element vertex')
-      num_vertices = line.split(' ')[2].to_i
-      num_vertices.times do
-        vertex = file.readline.split.map(&:to_f)
-        vertices << vertex
-      end
-    elsif line.start_with?('element face')
-      num_faces = line.split(' ')[2].to_i
-      num_faces.times do
-        face = file.readline.split.map(&:to_i)
-        faces << face
+  File.open(ply_file, 'r') do |file|
+    file.each_line do |line|
+      if line.start_with?('end_header')
+        reading_vertices = true
+      elsif reading_vertices
+        v = line.split.map(&:to_f)
+        vertices << "v #{v.join(' ')}"
+        reading_vertices = false if vertices.size == vertex_count
+      elsif reading_faces
+        f = line.split[1..-1].map { |i| i.to_i + 1 }.join(' ')
+        faces << "f #{f}"
+      elsif line.start_with?('element vertex')
+        vertex_count = line.split(' ')[-1].to_i
+      elsif line.start_with?('element face')
+        face_count = line.split(' ')[-1].to_i
+        reading_faces = true
       end
     end
   end
-end
 
-File.open(output_file, 'w') do |file|
-  vertices.each do |vertex|
-    file.write("v #{vertex[0]} #{vertex[1]} #{vertex[2]}\n")
-  end
-
-  faces.each do |face|
-    file.write("f #{face[1] + 1} #{face[2] + 1} #{face[3] + 1}\n")
+  File.open(obj_file, 'w') do |file|
+    vertices.each { |v| file.puts(v) }
+    faces.each { |f| file.puts(f) }
   end
 end
+
+ply_file = 'bunny.ply'
+obj_file = 'bunny.obj'
+ply_to_obj(ply_file, obj_file)
+
